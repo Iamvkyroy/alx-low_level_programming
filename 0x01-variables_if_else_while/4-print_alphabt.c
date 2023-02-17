7b9647609614d38d392ab16c51868202f6c25286
Iamvkyroy <itavkyroy@gmail.com>
2023-02-17 14:47:41 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - prints alpha in lower and upper case
+ * Return: Always 0
+ */
 int main(void)
 {
-char c;
+	char ch;
 
-c = 'a';
-while
-(c < = 'z') {
-((c != 'q' && c != 'e') && c < = 'z')
-putchar(c);
-c++;
-}
-putchar('\n');
-return (0);
+	for (ch = 'a'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
 }
