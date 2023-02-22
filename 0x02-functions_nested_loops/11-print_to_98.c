275bdd0f49365d00df141e81657e6d9634267b58
Iamvkyroy <itavkyroy@gmail.com>
2023-02-21 21:12:16 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,25 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 
 /**
  * print_to_98 - Prints all umbers from input to 98
  * @n: The starting number
+ * Return: 0 or 1
  */
 
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
-	}
-	else
-	{ while (n < 98)
-		printf("%d, ", n++);
-		printf("%d\n" n);
-	}
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+printf("98");
+putchar('\n');
 }
