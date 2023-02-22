d5f365d4a2addb38b06a787bf5f0cee74dcaa01e
Iamvkyroy <itavkyroy@gmail.com>
2023-02-22 05:29:28 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,6 @@ void print_times_table(int n)
 
 {
 int num, mult, prod;
-
 if (n >= 0 && n <= 15)
 {
 for (num = 0; num <= n; num++)
@@ -39,4 +38,6 @@ _putchar((prod % 10) + '0');
 _putchar('\n');
 }
 }
+}
+
 }
