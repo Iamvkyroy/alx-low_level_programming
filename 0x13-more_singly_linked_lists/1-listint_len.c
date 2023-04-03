97b556bd9341e479425369d326ec42445da59621
IamVkyroy <itavkyroy@gmail.com>
2023-04-03 13:18:50 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		num++;
+		nodes++;
 		h = h->nest;
 	}
 
-	return (num);
+	return (nodes);
 }
