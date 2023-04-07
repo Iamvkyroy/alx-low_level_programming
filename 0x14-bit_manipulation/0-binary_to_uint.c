47c5cb8b13b6fb76f212e976a6662aa0a5f590df
IamVkyroy <itavkyroy@gmail.com>
2023-04-07 09:38:17 -0700

    0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts the binary number to unsigned int
+ * binary_to_uint - converts the binary number to unsigned int
  * @b: the string containing the binary number
  *
  * Return: the number converted
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
+	int v;
 	unsigned int vkyroy = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (v = 0; b[v]; v++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[v] < '0' || b[v] > '1')
 			return (0);
-		vkyroy = 2 * vkyroy + (b[i] - '0');
+		vkyroy = 2 * vkyroy + (b[v] - '0');
 	}
 
 	return (vkyroy);
