de8a8bf0441e9418632233a0a9098b853d70e886
Iamvkyroy <itavkyroy@gmail.com>
2023-05-07 01:25:14 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index > 63)
+if (index >= sizeof(unsigned long int) * 8)
+{
 return (-1);
 
-*c = (~(1UL << index) & *c);
+*n &= ~(1UL << index);
 return (1);
 }
