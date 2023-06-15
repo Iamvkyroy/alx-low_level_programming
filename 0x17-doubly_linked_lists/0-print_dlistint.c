a792cc4c96e977f81db6041ee6def8a53a063818
Iamvkyroy <itavkyroy@gmail.com>
2023-06-15 10:26:13 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,7 +16,7 @@ size_t print_dlistint(const dlistint_t *h)
 	while (current != NULL)
 	{
 		printf("%i\n", current->n);
-		current = current>next;
+		current = current > next;
 		nbr_nodes++;
 	}
 
