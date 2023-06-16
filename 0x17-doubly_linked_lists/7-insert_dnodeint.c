7bfece2108d561d8e92f705e5fefd978e493d1e6
Iamvkyroy <itavkyroy@gmail.com>
2023-06-16 02:45:19 -0700

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,40 +9,57 @@
  * @n: The value of the new node
  * Return: The inserted node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new;
-
 	if (h == NULL)
-		return (0);
+		return NULL;
 
-	current = *h;
+	dlistint_t *current = *h;
 
-	while (idx != 0)
+	/* Special case: insert at the beginning (idx == 0) */
+	if (idx == 0)
 	{
+		dlistint_t *new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return NULL;
+
+		new->n = n;
+		new->prev = NULL;
+		new->next = current;
+
+		if (current != NULL)
+			current->prev = new;
+
+		*h = new; // Update the head pointer to the new node
+
+		return new;
+	}
+
+	/* Find the node at the specified index */
+	while (idx > 1)
+	{
+		if (current == NULL)
+			return NULL; /* Out of range */
+
 		current = current->next;
 		idx--;
-		if (current == NULL)
-			return (NULL);
 	}
 
-	new = malloc(sizeof(dlistint_t));
+	if (current == NULL)
+		return NULL; /* Out of range */
 
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+		return NULL;
 
 	new->n = n;
-	new->next = current;
-	new->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = new;
+	new->prev = current;
+	new->next = current->next;
 
-	/*TODO: Handle special case when idx is 0 and last index*/
+	if (current->next != NULL)
+		current->next->prev = new;
 
-	return (current);
+	current->next = new;
+
+	return new;
 }
